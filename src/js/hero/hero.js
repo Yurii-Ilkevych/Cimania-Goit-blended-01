99f8125612e1940c4e6a84d8e4cd8f4f7051bcc0
Yurii-Ilkevych <rufiktcl@gmail.com>
2023-06-04 14:18:21 +0300

    Update hero.js

--- src/js/hero/hero.js
+++ src/js/hero/hero.js
@@ -1,10 +1,10 @@
-const starRatingContainer = document.querySelector('.star-rating');
-const starIcons = starRatingContainer.querySelectorAll('.star-icon');
+// const starRatingContainer = document.querySelector('.star-rating');
+// const starIcons = starRatingContainer.querySelectorAll('.star-icon');
 
-const percentageRating = (rating / 5) * 100; // Assuming the rating is on a scale of 0 to 5
+// const percentageRating = (rating / 5) * 100; // Assuming the rating is on a scale of 0 to 5
 
-starIcons.forEach((star, index) => {
-  if (percentageRating >= (index + 1) * 20) {
-    star.classList.add('filled');
-  }
-});
+// starIcons.forEach((star, index) => {
+//   if (percentageRating >= (index + 1) * 20) {
+//     star.classList.add('filled');
+//   }
+// });
