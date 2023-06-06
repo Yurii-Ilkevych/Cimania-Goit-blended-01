0de992764bb43da616726936b00576be4bc4c8d5
VovaPliuto <vovaplyuto@gmail.com>
2023-06-06 14:20:39 +0300

    Update header and theme changer

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -2,15 +2,26 @@ const refs = {
   navMenuBtn: document.querySelector('.nav-menu-btn'),
   backdropEl: document.querySelector('.backdrop'),
   mobMenuEl: document.querySelector('.mobile-menu'),
+  logoTextEl: document.querySelector('.logo-text'),
   homeLinkEl: document.querySelectorAll('.home-link'),
   catalogLinkEl: document.querySelectorAll('.catalog-link'),
   libraryLinkEl: document.querySelectorAll('.library-link'),
+  themeChangerEl: document.getElementById('theme-changer'),
 };
 
 refs.navMenuBtn.addEventListener('click', onMenuBtnClick);
 refs.backdropEl.addEventListener('click', onBackdropClick);
+refs.themeChangerEl.addEventListener('change', onThemeChange);
 
+//  refs.homeLinkEl.forEach(link => {
+//    link.classList.remove('active');
+//  });
 addActiveClass();
+themeSetup();
+
+console.log(document.body);
+
+console.dir(refs.themeChangerEl.checked);
 
 function onMenuBtnClick(e) {
   mobileMenuToggle();
@@ -27,6 +38,23 @@ function mobileMenuToggle() {
   refs.backdropEl.classList.toggle('is-hidden-header');
   refs.mobMenuEl.classList.toggle('shown');
 }
+
+function onThemeChange(e) {
+  if (refs.themeChangerEl.checked) {
+    document.body.setAttribute("light", "");
+    localStorage.setItem("ui-theme", "light");
+  } else { 
+    document.body.removeAttribute("light");
+    localStorage.removeItem("ui-theme");
+  }
+}
+
+function themeSetup() { 
+  if (localStorage.getItem("ui-theme") === "light") { 
+    document.body.setAttribute('light', '');
+    refs.themeChangerEl.checked = true;
+  }
+} 
 
 function addActiveClass() {
   if (window.location.pathname.includes('index.html')) {
