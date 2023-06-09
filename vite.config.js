170d3d2159feafa665438740ef2b0504b4147b00
Yurii-Ilkevych <rufiktcl@gmail.com>
2023-06-09 16:27:51 +0300

    update optimizing

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import viteImagemin from "vite-plugin-imagemin"
-
+import viteImagemin from 'vite-plugin-imagemin';
 
 export default defineConfig({
   root: 'src',
@@ -28,15 +27,45 @@ export default defineConfig({
         optimizationLevel: 7,
       },
       mozjpeg: {
-        quality: 75,
+        quality: 20,
       },
       pngquant: {
-        quality: [0.75, 0.9],
+        quality: [0.8, 0.9],
         speed: 4,
       },
       svgo: {
-        plugins: [{ removeViewBox: false }, { removeXMLNS: true }],
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
       },
     }),
+    //  viteImagemin({
+    //   gifsicle: {
+    //     optimizationLevel: 7,
+    //     interlaced: false,
+    //   },
+    //   optipng: {
+    //     optimizationLevel: 7,
+    //   },
+    //   mozjpeg: {
+    //     quality: 75,
+    //   },
+    //   pngquant: {
+    //     quality: [0.75, 0.9],
+    //     speed: 4,
+    //   },
+    //   svgo: {
+    //     plugins: [{ removeViewBox: false }, { removeXMLNS: true }],
+    //   },
+    // }),
   ],
 });
+
+
+
